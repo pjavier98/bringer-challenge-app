239348f813c58f72b04328fd5f0155c40bbefe7b
Pedro Javier <pjpc@outlook.com.br>
2022-10-16 00:36:21 -0300

    create timeline component

--- src/pages/TrackEvents.tsx
+++ src/pages/TrackEvents.tsx
@@ -1,17 +1,78 @@
 // @mui
 import { styled } from '@mui/material/styles';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 // components
 import Page from '../components/Page';
+import { Box, Container, IconButton, Stack, TextField } from '@mui/material';
+import Iconify from 'src/components/Iconify';
+import { useState } from 'react';
 
 const RootStyle = styled('div')(({ theme }) => ({
   height: '100%',
 }));
 
 export default function TrackEventsPage() {
+  const [trackingNumber, setTrackingNumber] = useState('');
+
   return (
     <Page title="Track Events">
       <RootStyle>
-        <h1>Track Events</h1>
+        <Container sx={{ py: 5, alignItems: 'center', justifyContent: 'center' }}>
+          <Stack direction={'row'} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+            <TextField
+              size="small"
+              placeholder="Tracking Number"
+              value={trackingNumber}
+              onChange={(e) => setTrackingNumber(e.target.value.toLocaleUpperCase())}
+            />
+            <IconButton aria-label="search" color="primary" disabled={!trackingNumber}>
+              <Iconify icon={'eva:search-fill'} />
+            </IconButton>
+          </Stack>
+
+          <Box sx={{ mt: 5 }}>
+            <Timeline>
+              <TimelineItem>
+                <TimelineOppositeContent color="text.secondary">09:30 am</TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot color="success" />
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Eat</TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent color="text.secondary">10:00 am</TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Code</TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent color="text.secondary">12:00 am</TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Sleep</TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent color="text.secondary">9:00 am</TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Repeat</TimelineContent>
+              </TimelineItem>
+            </Timeline>
+          </Box>
+        </Container>
       </RootStyle>
     </Page>
   );
